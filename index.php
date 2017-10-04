ae13408d1cf1d867187617fe03cd7f416a2603fb
Shriya S Samak <sss329@afsconnect1.njit.edu>
2017-10-03 21:06:23 -0400

    added index file

--- index.php
+++ index.php
@@ -11,9 +11,24 @@ class main
 	public function construct()
 
 	{	
+	$date =  date('Y-m-d', time());
+	echo "The value of \$date: ".$date."<br>";
+
+	$tar = "2017/05/24";		
+	echo "The value of \$tar: ".$tar."<br>";
+
+	$year = array("2012", "396", "300","2000", "1100", "1089");
+	echo "The value of \$year: ";
+	print_r($year);
+
+	$this->html .= htmlTags::horizontalRule();
 
 
-	}	
+
+
+
+	}
+
 
 	public function destruct()
 
